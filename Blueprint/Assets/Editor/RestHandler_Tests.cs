4377f26d9655218fb8bcf0d02f5c1e3599138902
Adam Fox <af16371@my.bristol.ac.uk>
2018-11-22 15:24:00 +0000

    Add basic RestHandler and corresponding tests in RestHandler_Tests

---
+++ Blueprint/Assets/Editor/RestHandler_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using NUnit.Framework;
+using NUnit.Framework.Api;
+
+public class RestHandler_Tests {
+
+    [Test]
+    public void TestPerformGET()
+    {
+        var go = new GameObject();
+        var resthandler = go.AddComponent<RestHandler>();
+
+        var response = resthandler.performGET("http://jsonplaceholder.typicode.com/posts/1");
+        
+        JSONplaceholder inv = new JSONplaceholder();
+        inv = JsonUtility.FromJson<JSONplaceholder>(response);
+
+        Assert.That(inv.userId, Is.EqualTo(1));
+        Assert.That(inv.id, Is.EqualTo(1));
+        Assert.That(inv.title, Is.EqualTo("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+        Assert.That(inv.body, Is.EqualTo("quia et suscipit\nsuscipit recusandae consequuntur expedita et " +
+                                         "cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est " +
+                                         "autem sunt rem eveniet architecto"));
+    }
+}
+
+public class JSONplaceholder
+{
+    public int userId;
+    public int id;
+    public string title;
+    public string body;
+}
---
+++ Blueprint/Assets/Scripts/RestHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+public class RestHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Hello");
+		performGET("http://adamcfox.ddns.net:8000");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public string performGET(string url)
+	{
+		HttpWebRequest request = (HttpWebRequest) WebRequest.Create(String.Format(url));
+		HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+//		print(response.StatusDescription);
+		Stream stream = response.GetResponseStream();
+		StreamReader reader = new StreamReader(stream);
+
+		string str_response = reader.ReadToEnd();
+		print(str_response);
+		
+		reader.Close();
+		response.Close();
+		return str_response;
+	}
+}
+
+
