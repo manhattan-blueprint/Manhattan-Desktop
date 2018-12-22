d8d63eb764eebd47605375ee8749ae4fd13920c6
Jay Lees <jay.lees@me.com>
2018-12-22 21:34:13 +0000

    Add inventory, player pick up and put down

---
+++ Blueprint/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Behaviour for the UI item slots
+public class ItemSlot : MonoBehaviour {
+
+    public int id;
+    public Inventory inventory;
+    public ClickObject items;
+    private Vector3 position;
+    private Vector3 facing;
+    private int itemId;
+
+    void Start() {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        items = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<ClickObject>(); 
+    }
+
+    public void DropItem() {
+        foreach (Transform child in transform) {
+            position = Camera.main.gameObject.transform.position;
+            facing = Camera.main.gameObject.transform.forward * 2;
+            itemId = inventory.GetItems()[id].id;
+            GameObject.Destroy(child.gameObject);
+            inventory.GetItems()[id] = null;
+            switch (itemId) {
+                case (0):
+                    Transform cube = Instantiate(items.cube, new Vector3(position.x + facing.x , position.y + facing.y, position.z + facing.z), Quaternion.identity);
+                    cube.name = "Cube";
+                    break;
+                case (1):
+                    Transform cubeLarge = Instantiate(items.cubeLarge, new Vector3(position.x + facing.x , position.y + facing.y, position.z + facing.z), Quaternion.identity);
+                    cubeLarge.name = "Cube Large";
+                    break;
+                case (2):
+                    Transform capsule = Instantiate(items.capsule, new Vector3(position.x + facing.x , position.y + facing.y, position.z + facing.z), Quaternion.identity);
+                    capsule.name = "Capsule";
+                    break;
+                default:
+                    Debug.Log("The man who passes the sentence should swing the sword.");
+                    break;
+            }   
+        }
+    }
+}
+ 
--- Blueprint/Assets/Scripts/PlayerLook.cs
+++ Blueprint/Assets/Scripts/PlayerLook.cs
@@ -1,51 +1,52 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLook : MonoBehaviour {
-	[SerializeField] private string mouseXInputName, mouseYInputName;
-	[SerializeField] private float mouseSensitivity;
+    [SerializeField] private string mouseXInputName;
+    [SerializeField] private string mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform playerBody;
 
-	[SerializeField] private Transform playerBody;
+    private float xAxisClamp;
+    public bool paused = false; 
 
-	private float xAxisClamp;
+    private void Awake() {
+        LockCursor ();
+        xAxisClamp = 0;
+    }
+        
+    private void LockCursor() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-	private void Awake() {
-		LockCursor();
-		xAxisClamp = 0;
-	}
-		
-	private void LockCursor() {
-		Cursor.lockState = CursorLockMode.Locked;
-	}
+    private void Update() {
+        CameraRotation ();
+    }
 
-	private void Update() {
-		CameraRotation();
-	}
+    private void CameraRotation() {
+        float mouseX = Input.GetAxis (mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis (mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
-	private void CameraRotation() {
-		float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
-		float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+        xAxisClamp += mouseY;
 
-		xAxisClamp += mouseY;
+        if (xAxisClamp > 90.0f) {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampAxisRotationToValue (270.0f);
+        } else if (xAxisClamp < -90.0f) {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampAxisRotationToValue (90.0f);
+        }
 
-		if (xAxisClamp > 90.0f) {
-			xAxisClamp = 90.0f;
-			mouseY = 0.0f;
-			ClampAxisRotationToValue(270.0f);
-		} else if (xAxisClamp < -90.0f) {
-			xAxisClamp = -90.0f;
-			mouseY = 0.0f;
-			ClampAxisRotationToValue(90.0f);
-		}
+        transform.Rotate (Vector3.left * mouseY);
+        playerBody.Rotate (Vector3.up * mouseX);
+    }
 
-		transform.Rotate(Vector3.left * mouseY);
-		playerBody.Rotate(Vector3.up * mouseX);
-	}
-
-	private void ClampAxisRotationToValue(float value) {
-		Vector3 eulerRotation = transform.eulerAngles;
-		eulerRotation.x = value;
-		transform.eulerAngles = eulerRotation;
-	}
+    private void ClampAxisRotationToValue(float value) {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
 }
