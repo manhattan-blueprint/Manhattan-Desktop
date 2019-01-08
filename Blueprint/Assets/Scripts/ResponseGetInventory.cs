2bc5dc4bb76b4edcc25aed991ead49a0e7661117
Adam Fox <af16371@my.bristol.ac.uk>
2019-01-08 14:03:59 +0000

    Add inventory response object

---
+++ Blueprint/Assets/Scripts/ResponseGetInventory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class ResponseGetInventory {
+    public List<InventoryEntry> items;
+
+    public ResponseGetInventory(List<InventoryEntry> items) {
+        this.items = items;
+    }
+}
+
+[Serializable]
+public class InventoryEntry {
+    public int item_id;
+    public int quantity;
+
+    public InventoryEntry(int itemId, int quantity) {
+        item_id = itemId;
+        this.quantity = quantity;
+    }
+}
+
