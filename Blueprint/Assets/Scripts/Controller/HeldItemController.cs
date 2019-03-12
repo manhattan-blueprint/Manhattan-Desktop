2211f0ad2ed5cb4b502ead4d861986779191ba71
Adam Fox <af16371@my.bristol.ac.uk>
2019-03-12 15:46:36 +0000

    Add beginnings of held item UI, with supporting classes

---
+++ Blueprint/Assets/Scripts/Controller/HeldItemController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Controller;
+using Model;
+using Model.Redux;
+using Model.State;
+using UnityEngine;
+
+public class HeldItemController : MonoBehaviour, Subscriber<GameState> {
+    public InventoryItem heldItem;
+    private List<InventoryItem> placeableItems = new List<InventoryItem>();
+    private InventoryController inventoryController;
+    private HeldItemSlotController[] heldItemSlots;
+    
+    void Start() {
+        inventoryController = GameObject.Find("InventoryUICanvas").GetComponent<InventoryController>();
+        heldItemSlots = gameObject.GetComponentsInChildren<HeldItemSlotController>();
+    }
+
+    void Update() {
+        
+    }
+
+    public void StateDidUpdate(GameState gameState) {
+        List<InventoryItem> temp = new List<InventoryItem>();
+        
+        foreach (KeyValuePair<int, List<HexLocation>> element in gameState.inventoryState.inventoryContents) {
+            foreach(HexLocation loc in element.Value) {
+                // If can be placed 
+                if (inventoryController.GetItemType(element.Key) == 2) {
+                    InventoryItem item = new InventoryItem(inventoryController.GetItemName(element.Key), element.Key, loc.quantity);
+                    temp.Add(item);
+                } 
+            } 
+        }
+
+        placeableItems = temp;
+    }
+}
---
+++ Blueprint/Assets/Scripts/Controller/HeldItemSlotController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Model;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeldItemSlotController : MonoBehaviour {
+    private AssetManager assetManager;
+    private InventoryItem itemInSlot;
+    // TODO: move nullItem to a global location
+    private InventoryItem nullItem = new InventoryItem("", 0, 0);
+    
+    void Start() {
+        assetManager = AssetManager.Instance();
+    }
+
+    void Update() {
+        
+    }
+
+    public void setSlotItem(InventoryItem item) {
+        itemInSlot = item;
+        updateFields();
+    }
+
+    private void updateFields() {
+        Image image = transform.GetComponentsInChildren<Image>()[1];
+        Text text = transform.GetComponentInChildren<Text>();
+        
+        if (itemInSlot.GetId() != nullItem.GetId()) {
+            image.sprite = assetManager.GetItemSprite(itemInSlot.GetId());
+            text.text = itemInSlot.GetQuantity().ToString();
+                
+            image.enabled = true;
+            text.enabled = true;
+        } else {
+            image.enabled = false;
+            text.enabled = false;
+        }
+    }
+}
