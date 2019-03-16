58bfca26249848a2b22ef392beed9299f51c1904
Adam Fox <af16371@my.bristol.ac.uk>
2019-03-16 09:54:22 +0000

    Remove HeldItemSlotController (using InventorySlotController instead), removed unnecessary features from HeldItemController

--- Blueprint/Assets/Scripts/Controller/HeldItemController.cs
+++ Blueprint/Assets/Scripts/Controller/HeldItemController.cs
@@ -6,30 +6,14 @@ using Model.Redux;
 using Model.State;
 using UnityEngine;
 
-public class HeldItemController : MonoBehaviour, Subscriber<GameState> {
+public class HeldItemController : MonoBehaviour {
     public InventoryItem heldItem;
     private List<InventoryItem> placeableItems = new List<InventoryItem>();
     private InventoryController inventoryController;
-    private HeldItemSlotController[] heldItemSlots;
     
     void Start() {
         inventoryController = GameObject.Find("InventoryUICanvas").GetComponent<InventoryController>();
-        heldItemSlots = gameObject.GetComponentsInChildren<HeldItemSlotController>();
     }
 
-    public void StateDidUpdate(GameState gameState) {
-        List<InventoryItem> temp = new List<InventoryItem>();
-        
-        foreach (KeyValuePair<int, List<HexLocation>> element in gameState.inventoryState.inventoryContents) {
-            foreach(HexLocation loc in element.Value) {
-                // If can be placed 
-                if (inventoryController.GetItemType(element.Key) == 2) {
-                    InventoryItem item = new InventoryItem(inventoryController.GetItemName(element.Key), element.Key, loc.quantity);
-                    temp.Add(item);
-                } 
-            } 
-        }
-
-        placeableItems = temp;
-    }
+    // TODO: implement switching of held item (i.e. use of scroll wheel)
 }
