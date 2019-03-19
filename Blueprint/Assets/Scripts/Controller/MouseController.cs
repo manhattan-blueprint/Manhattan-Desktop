2ff76e3cfd369239c2072e2273e696273caacf43
Ben Lee <benlee98@live.com>
2019-03-19 22:25:23 +0000

    Make floor green and add more trees, add RMB destroy()

--- Blueprint/Assets/Scripts/Controller/MouseController.cs
+++ Blueprint/Assets/Scripts/Controller/MouseController.cs
@@ -55,6 +55,8 @@ namespace Controller {
 
                 if (!Physics.Raycast(ray, out hit)) return;
                 HexCell hc = hit.transform.gameObject.GetComponent<HexCell>();
+                Debug.Log(hc.getPosition());
+                Destroy(hit.transform.gameObject);
                 if (hc == null) return;
                 GameManager.Instance().inventoryStore.Dispatch(new RemoveHeldItem(hc.getPosition()));
             } 
