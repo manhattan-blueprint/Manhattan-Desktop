fc8350d84aceca8b84453afe698d3a47260fbb9b
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2019-03-26 12:22:34 +0000

    Fixed tab issue

--- Blueprint/Assets/Scripts/Service/Request/PayloadLeaderboard.cs
+++ Blueprint/Assets/Scripts/Service/Request/PayloadLeaderboard.cs
@@ -3,7 +3,7 @@ using Service.Response;
 using System.Collections.Generic;
 
 namespace Service.Request {
-    public class Item {
+public class Item {
         int item_id;
         public Item(int id) {
             item_id = id;
@@ -13,7 +13,7 @@ namespace Service.Request {
     public class PayloadLeaderboard {
         List<Item> blueprints;
 
-		public PayloadLeaderboard(int id) {
+        public PayloadLeaderboard(int id) {
             blueprints = new List<Item>();
             blueprints.Add(new Item(id));
 		}
