1eadd344e57cbfa4901c434c11f38c3265a9eb62
ben <benlee98@live.com>
2019-04-15 15:22:55 +0100

    Switch to daytime, add dynamic moving clouds, post-process lighting-effects

---
+++ Blueprint/Assets/Scripts/Controller/ExitGate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGate : MonoBehaviour
+{
+  void OnTriggerEnter(Collider other) {
+          Debug.Log("Its a hit!");
+       }
+}
