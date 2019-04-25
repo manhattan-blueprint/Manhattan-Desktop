b23334f636483e9e2eb5b7475ae65cb796e9b869
Jay Lees <jay.lees@me.com>
2019-04-25 14:25:21 +0100

    Tidy exit gate

--- Blueprint/Assets/Scripts/Controller/ExitGate.cs
+++ Blueprint/Assets/Scripts/Controller/ExitGate.cs
@@ -1,39 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Model;
+﻿using UnityEngine;
 using Model.Action;
-using Model.Redux;
 using Model.State;
 
-public class ExitGate : MonoBehaviour {
-  GameObject player;
-  float minDistance = 7;
+namespace Controller {
+    public class ExitGate : MonoBehaviour {
+        private GameObject player;
+        private const float minDistance = 7;
 
-  void Start() {
-    player = GameObject.Find("Player");
-  }
+        public void Start() {
+            player = GameObject.Find("Player");
+        }
 
-  void OnMouseOver() {
-      float dist = Vector3.Distance(player.transform.position, transform.position);
-      if (dist < minDistance && GameManager.Instance().uiStore.GetState().Selected == UIState.OpenUI.Playing) {
-          GameManager.Instance().uiStore.Dispatch(new OpenMouseUI());
-      } else if (dist > minDistance && GameManager.Instance().uiStore.GetState().Selected == UIState.OpenUI.Mouse) {
-          GameManager.Instance().uiStore.Dispatch(new CloseUI());
-      }
-  }
+        public void OnMouseOver() {
+            float dist = Vector3.Distance(player.transform.position, transform.position);
+            if (dist < minDistance && GameManager.Instance().uiStore.GetState().Selected == UIState.OpenUI.Playing) {
+                GameManager.Instance().uiStore.Dispatch(new OpenMouseUI());
+            } else if (dist > minDistance && GameManager.Instance().uiStore.GetState().Selected == UIState.OpenUI.Mouse) {
+                GameManager.Instance().uiStore.Dispatch(new CloseUI());
+            }
+        }
 
-  void OnMouseExit() {
-      float dist = Vector3.Distance(player.transform.position, transform.position);
-      if (GameManager.Instance().uiStore.GetState().Selected == UIState.OpenUI.Mouse) {
-          GameManager.Instance().uiStore.Dispatch(new CloseUI());
-      }
-  }
-
-  // void OnTriggerEnter(Collider other) {
-  //     GameManager.Instance().uiStore.Dispatch(new OpenMouseUI());
-  // }
-  // void OnTriggerExit(Collider other) {
-  //     GameManager.Instance().uiStore.Dispatch(new CloseUI());
-  // }
+        public void OnMouseExit() {
+            if (GameManager.Instance().uiStore.GetState().Selected == UIState.OpenUI.Mouse) {
+                GameManager.Instance().uiStore.Dispatch(new CloseUI());
+            }
+        }
+    }
 }
