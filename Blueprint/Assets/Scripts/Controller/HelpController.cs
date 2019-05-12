769c65c04e2bcf20d8375d700a0ca5fd76576e42
Will JV Smith <smithwjv@gmail.com>
2019-05-12 18:18:34 +0100

    Add initial backend of tutorial screen switching

---
+++ Blueprint/Assets/Scripts/Controller/HelpController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controller {
+    public class HelpController : MonoBehaviour {
+
+        [SerializeField] private TextMeshProUGUI helpTitle;
+        [SerializeField] private Button leftArrow;
+        [SerializeField] private Button exit;
+        [SerializeField] private Button rightArrow;
+
+        [SerializeField] private GameObject KeyBindingsGroup;
+        [SerializeField] private GameObject InventoryGroup;
+        [SerializeField] private GameObject PlacementGroup;
+        [SerializeField] private GameObject MachinesGroup;
+        [SerializeField] private GameObject BlueprintGroup;
+
+        enum HelpScreens {
+            KeyBindings,
+            Inventory,
+            Placement,
+            Machines,
+            Blueprint
+        };
+
+        private HelpScreens currentScreen = HelpScreens.KeyBindings;
+
+        void Start() {
+            
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void NextScreen() {
+            switch (currentScreen) {
+                case HelpScreens.KeyBindings:
+                    KeyBindingsGroup.SetActive(false);
+                    currentScreen = HelpScreens.Inventory;
+                    helpTitle.text = "Inventory";
+                    InventoryGroup.SetActive(true);
+                    leftArrow.enabled = true;
+                    break;
+                case HelpScreens.Inventory:
+                    InventoryGroup.SetActive(false);
+                    currentScreen = HelpScreens.Placement;
+                    helpTitle.text = "Placement";
+                    PlacementGroup.SetActive(true);
+                    break;
+                case HelpScreens.Placement:
+                    PlacementGroup.SetActive(false);
+                    currentScreen = HelpScreens.Machines;
+                    helpTitle.text = "Machines";
+                    MachinesGroup.SetActive(true);
+                    break;
+                case HelpScreens.Machines:
+                    MachinesGroup.SetActive(false);
+                    currentScreen = HelpScreens.Blueprint;
+                    helpTitle.text = "Blueprint Tree";
+                    BlueprintGroup.SetActive(true);
+                    rightArrow.enabled = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void PreviousScreen() {
+            switch (currentScreen) {
+                case HelpScreens.Inventory:
+                    InventoryGroup.SetActive(false);
+                    currentScreen = HelpScreens.KeyBindings;
+                    helpTitle.text = "Key Bindings";
+                    KeyBindingsGroup.SetActive(true);
+                    leftArrow.enabled = false;
+                    break;
+                case HelpScreens.Placement:
+                    PlacementGroup.SetActive(false);
+                    currentScreen = HelpScreens.Inventory;
+                    helpTitle.text = "Inventory";
+                    InventoryGroup.SetActive(true);
+                    break;
+                case HelpScreens.Machines:
+                    MachinesGroup.SetActive(false);
+                    currentScreen = HelpScreens.Placement;
+                    helpTitle.text = "Placement";
+                    PlacementGroup.SetActive(true);
+                    break;
+                case HelpScreens.Blueprint:
+                    BlueprintGroup.SetActive(false);
+                    currentScreen = HelpScreens.Machines;
+                    helpTitle.text = "Machines";
+                    MachinesGroup.SetActive(true);
+                    rightArrow.enabled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
